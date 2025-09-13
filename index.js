eb8a3151605f7eb4be9f151d137b9ca873a1e814
Justin <zorrotab@gmail.com>
2025-09-13 16:36:03 +1000

    Add demo static site

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+    const express = require('express');
+    const app = express();
+    const port = 8080; // Or any desired port
+
+    //app.get('/', (req, res) => {
+    //  res.send('Hello World!');
+    //});
+
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    });
+
+    app.use(express.static('site')); // Assuming your static files are in a folder named 'public'
